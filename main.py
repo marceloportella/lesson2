f9ca33cdaeb9422453f9ed1aa0cd3b478da0537a
Marcelo Portella <marcelo.portella@outlook.com>
2025-01-10 17:19:31 -0300

    exercicios

---
+++ main.py
@@ -0,0 +1,17 @@
+import math
+# nome='Marcelo'
+
+# print(type(nome))
+
+# print(nome.split("r")) # O que vem antes e o que vem depois
+
+# numero1 = int(input("Digite o primeiro numero: "))
+# numero2 = int(input("Digite o segundo numero: "))
+
+# divisao = numero1 // numero2
+
+# print (f"O resultado é: {divisao}")
+
+raio = float(input("Digite o raio: "))
+area_do_circulo = math.pi * raio ** 2
+print(f"{area_do_circulo:.2f}")
