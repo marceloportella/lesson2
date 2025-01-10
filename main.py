438d6aa88dd2b5f8e849d8ef025e2bd030b74e76
Marcelo Portella <marcelo.portella@outlook.com>
2025-01-10 17:43:08 -0300

    segundo commit

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ import math
 
 # print (f"O resultado é: {divisao}")
 
-raio = float(input("Digite o raio: "))
-area_do_circulo = math.pi * raio ** 2
-print(f"{area_do_circulo:.2f}")
+# raio = float(input("Digite o raio: "))
+# area_do_circulo = math.pi * raio ** 2
+# print(f"{area_do_circulo:.2f}")
+
+try:
+    numero_01 = int(input("Digite o primeiro numero: "))
+    numero_02 = int(input("Digite o segundo numero: "))
+    resultado = numero_01/numero_02
+    print("resultado")
+except TypeError as e:
+    print(e)
+else:
+    print("deu certo")
+finally:
+    print("independente do que acontecer sempre vou aparecer, esse é o finally")
